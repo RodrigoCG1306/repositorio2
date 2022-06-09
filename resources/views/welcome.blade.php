bd85bb288711b98ac5ed886a88111ed6fbea31c4
RodrigoCG1306 <cazares.jose.gm@gmail.com>
2022-06-09 11:35:55 -0500

    Actualización plantilla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Rodrigo Master</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -16,6 +16,7 @@
 
         <style>
             body {
+                font-size: 18px;
                 font-family: 'Nunito', sans-serif;
             }
         </style>
